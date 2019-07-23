6c227ea93787e7508b09dc8457c800631dc23ba1
zhengdengyao <zhengdengyao@51yryc.com>
2019-07-23 17:31:06 +0800

    完成精选ui

---
+++ app/src/main/java/com/example/chenghejianzhi/adapter/RecommendAdapter.java
@@ -0,0 +1,148 @@
+package com.example.chenghejianzhi.adapter;
+
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.base.base.BaseRecyclerAdapter;
+import com.example.chenghejianzhi.R;
+import com.example.chenghejianzhi.utils.DimenUtil;
+import com.example.chenghejianzhi.view.GlideImageLoader;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * @author : sklyand
+ * @email : zhengdengyao@51yryc.com
+ * @time : 2019/7/23 11:36
+ * @describe ：
+ */
+public class RecommendAdapter extends BaseRecyclerAdapter<BaseRecyclerAdapter.RecyclerItem> {
+    public final static int TOP_BANNER = 0;
+    public final static int RECOMMEND = 1;
+    public final static int TITLE = 2;
+    @Override
+    protected int getItemViewType(int position, RecyclerItem recyclerItem) {
+        switch (recyclerItem.type){
+            case TOP_BANNER:
+                return R.layout.item_recommend_banner;
+            case RECOMMEND:
+                return R.layout.item_recommend_today;
+            case TITLE:
+                return R.layout.item_home_title;
+        }
+
+        return R.layout.item_home_hot_recommend;
+    }
+
+    @Override
+    protected ViewHolder<RecyclerItem> onCreateViewHolder(View root, ViewGroup parent, int viewType) {
+        switch (viewType){
+            case  R.layout.item_recommend_banner:
+                return new BannerHolder(root);
+            case R.layout.item_home_title:
+                return new TitleViewHolder(root);
+            case R.layout.item_recommend_today:
+                return new RecommendViewHolder(root);
+        }
+        return new RecommendViewHolder(root);
+    }
+
+    /**
+     * 头部三个按钮
+     */
+    public class BannerHolder extends ViewHolder<RecyclerItem>{
+        @BindView(R.id.banner)
+        Banner banner;
+
+
+        public BannerHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(RecyclerItem recyclerItem) {
+            //设置图片加载器
+            banner.setImageLoader(new GlideImageLoader());
+            //设置图片集合
+            List<Integer> images = new ArrayList<>();
+            for (int i = 0;i<3;i++){
+             images.add(R.drawable.item_home_hot_bg_test);
+            }
+            banner.setImages(images);
+            //banner设置方法全部调用完毕时最后调用
+            banner.start();
+        }
+    }
+
+    /**
+     * 热门活动
+     */
+    public class HotViewHolder extends ViewHolder<RecyclerItem>{
+        @BindView(R.id.recycler_hot_activity)
+        RecyclerView recyclerView;
+
+        public HotViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(RecyclerItem recyclerItem) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(itemView.getContext(),LinearLayoutManager.HORIZONTAL,false));
+            recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+                @Override
+                public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                    outRect.right = DimenUtil.dipToPixels(itemView.getContext(),8);
+                }
+            });
+            HomeHotActivityAdapter homeHotActivityAdapter = new HomeHotActivityAdapter();
+            recyclerView.setAdapter(homeHotActivityAdapter);
+            List<String> hotList = new ArrayList<>();
+            for (int i = 0;i<10;i++){
+                hotList.add("");
+            }
+            homeHotActivityAdapter.replace(hotList);
+        }
+    }
+    /**
+     * 热门推荐
+     */
+    public class RecommendViewHolder extends ViewHolder<RecyclerItem>{
+
+
+        public RecommendViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(RecyclerItem recyclerItem) {
+
+        }
+    }
+    /**
+     * 标题
+     */
+    public class TitleViewHolder extends ViewHolder<RecyclerItem>{
+        @BindView(R.id.tv_title)
+        TextView tv_title;
+
+        public TitleViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(RecyclerItem recyclerItem) {
+            if (recyclerItem.data!=null){
+                tv_title.setText((String)recyclerItem.data);
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/chenghejianzhi/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/chenghejianzhi/fragments/RecommendFragment.java
@@ -1,15 +1,21 @@
 package com.example.chenghejianzhi.fragments;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.example.base.base.BaseFragment;
 import com.example.base.base.BaseMvpFragment;
 import com.example.base.base.BaseRecyclerAdapter;
 import com.example.chenghejianzhi.R;
+import com.example.chenghejianzhi.adapter.RecommendAdapter;
 import com.example.chenghejianzhi.contract.RecommendContract;
 import com.example.chenghejianzhi.presenter.RecommendPresenter;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * @author : sklyand
@@ -18,6 +24,8 @@ import java.util.List;
  * @describe ：
  */
 public class RecommendFragment extends BaseMvpFragment<RecommendContract.Presenter> implements RecommendContract.View {
+    @BindView(R.id.recycler_recommend)
+    RecyclerView recycler_recommend;
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_recommend;
@@ -25,7 +33,16 @@ public class RecommendFragment extends BaseMvpFragment<RecommendContract.Present
 
     @Override
     protected void initWidget(View root) {
-
+        recycler_recommend.setLayoutManager(new LinearLayoutManager(getContext()));
+        RecommendAdapter recommendAdapter = new RecommendAdapter();
+        recycler_recommend.setAdapter(recommendAdapter);
+        List<BaseRecyclerAdapter.RecyclerItem> itemList = new ArrayList<>();
+        itemList.add(new BaseRecyclerAdapter.RecyclerItem(RecommendAdapter.TOP_BANNER,null));
+        itemList.add(new BaseRecyclerAdapter.RecyclerItem(RecommendAdapter.TITLE,"今日精选"));
+        for (int i = 0;i<10;i++){
+            itemList.add(new BaseRecyclerAdapter.RecyclerItem(RecommendAdapter.RECOMMEND,null));
+        }
+        recommendAdapter.replace(itemList);
     }
 
     @Override
