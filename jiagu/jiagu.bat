4aec9890b37b4b181777cb1c16a86a4f337471ee
zhengdengyao <zhengdengyao@51yryc.com>
2019-08-16 15:52:25 +0800

    增加360加固

---
+++ jiagu/jiagu.bat
@@ -0,0 +1,14 @@
+set APK=%1
+set DEST=%2
+set IS_CHANNEL=%3
+echo %APK%
+echo "------ jiagu  running..."
+
+md %DEST%
+java -jar ../jiagu/jiagu.jar -login FantasySkyland zdy19941209
+java -jar ../jiagu/jiagu.jar -importsign ../app/keystore.jks jz1234 keyJz jz1234
+java -jar ../jiagu/jiagu.jar -showmulpkg
+java -jar ../jiagu/jiagu.jar -config -crashlog -x86 -msg
+if %IS_CHANNEL% ==1 (java -jar ../jiagu/jiagu.jar -jiagu %APK% %DEST% -autosign -pkgparam ../jiagu/channels.txt -automulpkg) else (java -jar ../jiagu/jiagu.jar -jiagu %APK% %DEST% -autosign)
+
+echo "------ jiagu  finished!"
