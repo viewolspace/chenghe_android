a57071cf6bb23719c57c7b33c39536a353400f8c
zhengdengyao <zhengdengyao@51yryc.com>
2019-12-27 18:47:57 +0800

    首页ui修改

--- app/src/main/java/com/parttime/potato/view/GlideImageLoader.java
+++ app/src/main/java/com/parttime/potato/view/GlideImageLoader.java
@@ -2,11 +2,13 @@ package com.parttime.potato.view;
 
 import android.content.Context;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.parttime.potato.R;
+import com.parttime.potato.utils.PhoneUtils;
 import com.youth.banner.loader.ImageLoader;
 
 /**
@@ -20,7 +22,17 @@ public class GlideImageLoader extends ImageLoader {
     public void displayImage(Context context, Object path, ImageView imageView) {
         imageView.setScaleType(ImageView.ScaleType.FIT_XY);
         Glide.with(context).load(path)
-//                .apply(new RequestOptions().transform(new RoundedCorners(25)))
+                .apply(new RequestOptions().transform(new RoundedCorners(25)))
                 .into(imageView);
+    }
+
+    @Override
+    public ImageView createImageView(Context context) {
+        ImageView imageView = new ImageView(context);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(PhoneUtils.dipToPixels(330),RelativeLayout.LayoutParams.MATCH_PARENT);
+        layoutParams.leftMargin = PhoneUtils.dipToPixels(15);
+        layoutParams.rightMargin = PhoneUtils.dipToPixels(15);
+        imageView.setLayoutParams(layoutParams);
+        return super.createImageView(context);
     }
 }
