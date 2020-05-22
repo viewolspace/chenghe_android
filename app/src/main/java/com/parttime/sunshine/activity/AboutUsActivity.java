1b5f4138b69ff2b73472a6a061d790171342fe65
zhengdengyao <zhengdengyao@51yryc.com>
2020-05-22 16:29:23 +0800

    关于我们变成动态qq

--- app/src/main/java/com/parttime/sunshine/activity/AboutUsActivity.java
+++ app/src/main/java/com/parttime/sunshine/activity/AboutUsActivity.java
@@ -2,20 +2,35 @@ package com.parttime.sunshine.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.parttime.base.base.BaseActivity;
+import com.parttime.base.bean.ContactBean;
+import com.parttime.base.constants.Constants;
+import com.parttime.base.retrofit.ApiService;
+import com.parttime.base.retrofit.RetrofitServiceCreator;
 import com.parttime.base.rx.RxEvent;
+import com.parttime.base.rx.RxThrowableConsumer;
+import com.parttime.base.rx.RxUtils;
 import com.parttime.sunshine.R;
 import com.parttime.sunshine.utils.StatusBarUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.functions.Consumer;
 
 public class AboutUsActivity extends BaseActivity {
-    public static void start(Context context){
-        Intent intent = new Intent(context,AboutUsActivity.class);
+    @BindView(R.id.tv_qq)
+    TextView tvQq;
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, AboutUsActivity.class);
         context.startActivity(intent);
     }
+
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_about_us;
@@ -28,8 +43,11 @@ public class AboutUsActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-
+        RetrofitServiceCreator.createService(this, ApiService.class).getAppQQ(Constants.APP)
+                .compose(RxUtils.rxSchedulerHelper())
+                .compose(mProvider.bindToLifecycle()).subscribe(contactBean -> tvQq.setText(contactBean.getShowMsg()),new RxThrowableConsumer());
     }
+
     @OnClick({R.id.iv_back})
     public void onClick(View view) {
         switch (view.getId()) {
@@ -38,8 +56,17 @@ public class AboutUsActivity extends BaseActivity {
                 break;
         }
     }
+
     @Override
     public void handleDefaultEvent(RxEvent event) {
 
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
 }
