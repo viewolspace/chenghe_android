cd211643e8eab6e5ea864ce22d6020277e9ac99d
zhengdengyao <zhengdengyao@51yryc.com>
2020-06-16 10:21:24 +0800

    增加热云打点

--- app/src/main/java/com/parttime/songshu/PartTimeApplication.java
+++ app/src/main/java/com/parttime/songshu/PartTimeApplication.java
@@ -1,6 +1,8 @@
 package com.parttime.songshu;
 
 import com.parttime.base.base.App;
+import com.reyun.tracking.sdk.Tracking;
+import com.umeng.analytics.AnalyticsConfig;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 
@@ -18,7 +20,8 @@ public class PartTimeApplication extends App {
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
         // 支持在子进程中统计自定义事件
         UMConfigure.setProcessEvent(true);
-
+        Tracking.setDebugMode(true);
+        Tracking.initWithKeyAndChannelId(this,"fe92381bedbf6a5a326fd426490d6c1e", AnalyticsConfig.getChannel(this));
         //UMConfigure.setLogEnabled(true);
     }
 }
