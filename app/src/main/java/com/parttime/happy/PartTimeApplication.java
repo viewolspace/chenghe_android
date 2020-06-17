2d4130a655984ffa326176f44392203f1f2b5ee4
zhengdengyao <zhengdengyao@51yryc.com>
2020-06-17 15:22:43 +0800

    增加热云事件统计

--- app/src/main/java/com/parttime/happy/PartTimeApplication.java
+++ app/src/main/java/com/parttime/happy/PartTimeApplication.java
@@ -1,6 +1,8 @@
 package com.parttime.happy;
 
 import com.parttime.base.base.App;
+import com.reyun.tracking.sdk.Tracking;
+import com.umeng.analytics.AnalyticsConfig;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 
@@ -18,7 +20,7 @@ public class PartTimeApplication extends App {
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
         // 支持在子进程中统计自定义事件
         UMConfigure.setProcessEvent(true);
-
+        Tracking.initWithKeyAndChannelId(this,"2f7a5e98b2af38f6f464da6eef9b5834", AnalyticsConfig.getChannel(this));
         //UMConfigure.setLogEnabled(true);
     }
 }
