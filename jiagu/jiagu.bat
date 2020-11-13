717af4349fd46f561cd9f604459c524ff0f5d2b5
wangdong <wangdong@youguu.com>
2020-11-13 16:32:31 +0800

    修改签名

--- jiagu/jiagu.bat
+++ jiagu/jiagu.bat
@@ -6,7 +6,7 @@ echo "------ jiagu  running..."
 
 md %DEST%
 java -jar ../jiagu/jiagu.jar -login FantasySkyland zdy19941209
-java -jar ../jiagu/jiagu.jar -importsign ../app/keystore.jks jz1234 keyJz jz1234
+java -jar ../jiagu/jiagu.jar -importsign ../app/keystore.jks 123456 kljz 123456
 java -jar ../jiagu/jiagu.jar -showmulpkg
 java -jar ../jiagu/jiagu.jar -config -crashlog -x86 -msg
 if %IS_CHANNEL% ==1 (java -jar ../jiagu/jiagu.jar -jiagu %APK% %DEST% -autosign -pkgparam ../jiagu/channels.txt -automulpkg) else (java -jar ../jiagu/jiagu.jar -jiagu %APK% %DEST% -autosign)
